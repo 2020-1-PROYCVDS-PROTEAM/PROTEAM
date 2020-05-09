83b7d3768cb9956041c00d6992e47b99c4f736a6
csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2020-05-08 21:57:55 -0500

    Arreglando frontend y tabla

--- src/main/java/edu/eci/cvds/samples/Services/client/Main.java
+++ src/main/java/edu/eci/cvds/samples/Services/client/Main.java
@@ -6,11 +6,11 @@ import edu.eci.cvds.samples.Services.ServicioProteamFactory;
 public class Main {
 
     public static void main(String[] args) throws ServicesException{
-        System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarUsuarios());
+        //System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarUsuarios());
         //System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarUsuario("jotaroj"));
     	//System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarIniciativas());
     	//System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().palabraClaveIniciativa("S"));
-    	//System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarIniciativasProponente("Jotaro"));
+    	System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarIniciativasProponente("jotaroj"));
         System.exit(0);
     }
 
