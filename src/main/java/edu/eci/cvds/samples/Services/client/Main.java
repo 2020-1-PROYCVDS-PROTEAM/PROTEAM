f503741365bad0631e0d2b9b7eab226f17543920
csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2020-05-08 20:57:43 -0500

    Tareas #61 y #60 vista pagina y pagina proponente

--- src/main/java/edu/eci/cvds/samples/Services/client/Main.java
+++ src/main/java/edu/eci/cvds/samples/Services/client/Main.java
@@ -6,11 +6,11 @@ import edu.eci.cvds.samples.Services.ServicioProteamFactory;
 public class Main {
 
     public static void main(String[] args) throws ServicesException{
-        //System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarUsuarios());
+        System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarUsuarios());
         //System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarUsuario("jotaroj"));
     	//System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarIniciativas());
     	//System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().palabraClaveIniciativa("S"));
-    	System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarIniciativasProponente("jotaroj"));
+    	//System.out.println(ServicioProteamFactory.getInstance().getServicioProteam().consultarIniciativasProponente("Jotaro"));
         System.exit(0);
     }
 
